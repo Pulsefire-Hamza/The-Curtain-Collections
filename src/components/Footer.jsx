ab03b65e3db4594faf5b46cef0da783d03548835
Hamza Tariq <140837284+Pulsefire-Hamza@users.noreply.github.com>
2024-12-16 15:33:20 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
         <div className="w-full md:w-1/3 flex flex-col items-center md:items-start space-y-8">
           <div className="bg-white p-4 rounded-full shadow-lg">
             <img
-              src="https://firebasestorage.googleapis.com/v0/b/furniture-storage.appspot.com/o/logo.png?alt=media&token=0fe12fc6-a71f-48d6-961d-641c2b141e89"
+              src="https://firebasestorage.googleapis.com/v0/b/furniture-storage.appspot.com/o/mainlogo-removebg-preview.png?alt=media&token=2189409f-d805-4131-849c-9a9c49e2baf7"
               alt="Logo"
               className="w-32 h-32 md:w-40 md:h-40"
             />
